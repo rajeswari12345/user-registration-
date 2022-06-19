36a72091931aff41bd82cee0fc0f644b0133041d
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 19:25:15 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPattternProblems;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("User Registration using regular expression");
+        RegexPatttern regexpattern = new RegexPattern();
+        regexpattern.validating();
+
+
+    }
+}
